675f0f40fbffcf395e8aab92ccafe7b0d390f39a
heriawan <heriawan@aplog.co>
2023-10-16 18:42:01 +0700

    web upd

--- assets/script.js
+++ assets/script.js
@@ -5,86 +5,102 @@ const rm_el=(parentelement,selector)=>{
         });
     }else{ parentelement.querySelectorAll(selector).forEach((el,i)=>{ el.remove(); }); }
 }
-const dump=(x)=>{
-    var v=Object.keys({x}); console.log(v,`:${x}`);
-}
 const mkParallax = (prm)=>{
     var windowHeight = window.innerHeight, 
-    windowHeightExtra = 0, 
-    safari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent),
-    mobile = /Mobi/.test(navigator.userAgent);
-    dump(windowHeight);
-    safari && 
-    !mobile && 
-    (windowHeightExtra = window.outerHeight - window.innerHeight),
-    mobile && (
-        windowHeight = window.screen.availHeight,
-        windowHeightExtra = (window.screen.availHeight - window.innerHeight) / 2
-    );
-
+        windowHeightExtra = 0, 
+        safari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent),
+        mobile = /Mobi/.test(navigator.userAgent);
+    
+    safari &&  !mobile && 
+        (windowHeightExtra = window.outerHeight - window.innerHeight),
+    mobile && 
+        (
+            windowHeight = window.screen.availHeight,
+            windowHeightExtra = (window.screen.availHeight - window.innerHeight) / 2
+        );
+    
     let PLX={
         par:{ speed: 1.5, className: "prlx"},
-        paraPos: (e, t, a, i) =>{
-            var n = e.top / t - windowHeightExtra;
-            a[i].style.top = n + "px"
-        }, paraAnim : (e, t) => {
-            for (var a = 0; e.length > a; a++) {
-                var i = e[a].parentElement.parentElement.getBoundingClientRect();
-                0 <= i.top + i.height && i.top <= windowHeight && PLX.paraPos(i, t, e, a)
-            }
-        }, hgCalc: (e, t) => {
-            for (var a = 0; e.length > a; a++) {
-                var i = e[a].parentElement.parentElement.getBoundingClientRect(), 
-                n = e[a].parentElement.parentElement.offsetTop, 
+        hgCalc: (el) => {
+            for (var x = 0; el.length > x; x++) {
+                var i = el[x].parentElement.parentElement.getBoundingClientRect(), 
+                speed=el[x].parentElement.getAttribute("speed"),
+                n = el[x].parentElement.parentElement.offsetTop, 
                 o = (windowHeight - i.height) / 2,
                 r = windowHeight > i.height + n 
-                    ? i.height + n - n / t 
-                    : i.height + 2 * (o - o / t);
-                e[a].style.height = r + 2 * windowHeightExtra + "px",
-                PLX.paraPos(i, t, e, a)
+                    ? i.height + n - n / speed 
+                    : i.height + 2 * (o - o / speed);
+                el[x].style.height = r + 2 * windowHeightExtra + "px",
+                PLX.paraPos(el[x], i, speed)
             }
-        }, init: ()=>{
-            for (var n = document.getElementsByClassName(PLX.par.className), e = 0; n.length > e; e++) {
+        }, getScrollElm:(node)=>{
+            if (node == null) { return null; }
+            if (node.scrollHeight > node.clientHeight) { return node; } 
+            else { return PLX.getScrollElm(node.parentNode); }
+        }, init: (el)=>{
+            for (x = 0; x < el.length; x++) {
                 var t = document.createElement("div");
-                n[e].parentNode.insertBefore(t, n[e]),
-                t.appendChild(n[e]);
-                var a = n[e].parentElement;
+                el[x].parentNode.insertBefore(t, el[x]),
+                t.appendChild(el[x]);
+                var a = el[x].parentElement;
                 a.className += `${PLX.par.className}__cont`,
-                "relative" !== window.getComputedStyle(a.parentElement, null).getPropertyValue("position") && (a.parentElement.style.position = "relative");
-                var o = n[e].dataset.imgSrc;
-                void 0 !== o && (n[e].style.backgroundImage = `url(${o})`,
-                1 === n[e].classList.length && PLX.par.className === n[e].classList[0] && (n[e].style.backgroundRepeat = "no-repeat",
-                n[e].style.backgroundPosition = "center",
-                n[e].style.backgroundSize = "cover"))
+                a.setAttribute('speed',el[x].dataset.speed==void 0?PLX.par.speed:el[x].dataset.speed),
+                "relative" !== window.getComputedStyle(a.parentElement, null).getPropertyValue("position") && 
+                    (a.parentElement.style.position = "relative");
+                var o = el[x].dataset.imgSrc;
+                el[x].removeAttribute('data-speed'),el[x].removeAttribute('data-img-src');
+                void 0 !== o && 
+                (
+                    el[x].style.backgroundImage = `url(${o})`,
+                    1 === el[x].classList.length && PLX.par.className === el[x].classList[0] && 
+                    (   el[x].style.backgroundRepeat = "no-repeat",
+                        el[x].style.backgroundPosition = "center",
+                        el[x].style.backgroundSize = "cover")
+                )
             }
             document.addEventListener("readystatechange", (e) => {
-                var t, a;
+                var scel=PLX.getScrollElm(el[0]).nodeName,
+                    scrollOnBody=scel==="BODY";
                 "complete" === e.target.readyState && (
-                    t = n,
-                    (a = PLX.par.speed) < 1 && (a = 1),
-                    PLX.hgCalc(t, a),
+                    PLX.hgCalc(el),
                     mobile || window.addEventListener("resize", () => {
                         windowHeight = window.innerHeight,
-                        PLX.hgCalc(t, a)
+                        PLX.hgCalc(el)
                     }),
-                    window.addEventListener("scroll", ()=>{ PLX.paraAnim(t, a) })
+                    scrollOnBody && window.addEventListener("scroll", () => { PLX.paraAnim(el) }),
+                    !scrollOnBody && document.querySelector(scel.toLowerCase()).addEventListener("scroll", () => {
+                        PLX.paraAnim(el)  
+                    })
                 )
+                
             })
+        }, paraAnim : (el) => {
+            for (var x = 0; x<el.length; x++) { 
+                var i = el[x].parentElement.parentElement.getBoundingClientRect();
+                var speed=el[x].parentElement.getAttribute("speed");
+                0 <= i.top + i.height && i.top <= windowHeight && 
+                    PLX.paraPos(el[x], i, speed)
+            }
+        },
+        paraPos: (elm, e, speed ) =>{
+            var c = e.top / speed - windowHeightExtra;
+            elm.style.top = c + "px"
         }
     };
+    
     PLX.par=Object.assign(PLX.par,prm);
-    PLX.init();
+    var el = document.getElementsByClassName(PLX.par.className);
+    PLX.init(el);
 };
 document.addEventListener("DOMContentLoaded", ()=>{
-    mkParallax({ speed: 3 });	
     document.querySelectorAll("[href^='#']").forEach(x=>{
         x.addEventListener('click',e=>{
             try{
                 var to=e.target.href.split("#")[1];
-                console.log(`doing scroll to "${to}"`)
                 document.getElementById(to).scrollIntoView({behavior: "smooth"});
                 e.preventDefault();
             }catch(xc){ }
         });
     });
+    mkParallax({ speed: 3 });	
 });
